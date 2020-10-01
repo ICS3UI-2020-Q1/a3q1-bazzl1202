ea5f062d6cb184bf19d54086aa85843ac776d69a
bazzl1202 <bazzl1202@wrdsb.ca>
2020-10-01 14:03:18 +0000

    Now determines the maximum integer

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Finds the maximum number out of three numbers.
+ * @author Liana Bazzarella
  */
 public class Main {
 
@@ -9,7 +10,27 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // ask the user to input three integers
+    System.out.println("Please enter three integers on separate lines");
+
+    // delclare and initialize the three integers
+    int integerOne = input.nextInt();
+    int integerTwo = input.nextInt();
+    int integerThree = input.nextInt(); 
+
+    // determining the maximum integer
+    if (integerOne >= integerTwo && integerOne >= integerThree){
+      System.out.println("Maximun: " + integerOne);
+    }else if(integerTwo >= integerOne && integerTwo >= integerThree){
+      System.out.println("Maximun: " + integerTwo);
+    }else if(integerThree >= integerOne && integerThree >= integerTwo){
+      System.out.println("Maximum: " + integerThree);
+    }
+
+
     
   }
 }
